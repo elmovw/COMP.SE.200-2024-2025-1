f873015a29f29386de624c08fbcdac90a1779872
Valhe Kouneli <valhe.kouneli@gmail.com>
2024-11-30 15:51:44 +0200

    Unit tests for clamp and chunk Remove files not belonging to version control

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -3,9 +3,14 @@ import chunk from '../src/chunk.js'
 
 
 describe('chunk()', function () {
-    it('todo')
 
-    // it('1 + 1 = 2', function () {
-    //     assert.equal(add(1, 1), 2);
-    // });
+    it('chunks array of numbers', function () {
+        const res = chunk([1, 2, 3, 4, 5, 6, 7], 2);
+        assert.deepEqual(res, [[1, 2], [3, 4], [5, 6], [7]]);
+    });
+
+    it('chunks array of letters', function () {
+        const res = chunk(['a', 'b', 'c', 'd'], 2);
+        assert.deepEqual(res, [['a', 'b'], ['c', 'd']]);
+    })
 });
--- test/clamp.test.js
+++ test/clamp.test.js
@@ -3,9 +3,33 @@ import clamp from '../src/clamp.js'
 
 
 describe('clamp()', function () {
-    it('todo')
+    it('clamps negative input number outside of limits between negative limit and positive limit', function () {
+        const res = clamp(-10, -5, 5)
+        assert.deepEqual(res, -5);
+    });
 
-    // it('1 + 1 = 2', function () {
-    //     assert.equal(add(1, 1), 2);
-    // });
+    it('clamps positive input number outside of limits between negative limit and positive limit', function () {
+        clamp(10, -5, 5);
+        assert.deepEqual(res, 5);
+    });
+
+    it('clamps negative input between negative limit and positive limit to itself', function () {
+        const res = clamp(-2, -5, 5)
+        assert.deepEqual(res, -2);
+    });
+
+    it('clamps positive input between negative limit and positive limit to itself', function () {
+        const res = clamp(2, -5, 5)
+        assert.deepEqual(res, 2);
+    });
+
+    it('clamps lower limit of negative limit and positive limit to itself', function () {
+        const res = clamp(-5, -5, 5)
+        assert.deepEqual(res, -5);
+    });
+
+    it('clamps upper limit of negative limit and positive limit to itself', function () {
+        const res = clamp(5, -5, 5)
+        assert.deepEqual(res, 5);
+    });
 });
