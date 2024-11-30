ca8bc0060161b43fec890e32441f4dced20dac2b
Valhe Kouneli <valhe.kouneli@gmail.com>
2024-11-30 18:43:50 +0200

    add tests for map

--- test/map.test.js
+++ test/map.test.js
@@ -1,10 +1,23 @@
-import assert from 'node:assert'
-import map from '../src/map.js'
+import map from '../src/map.js';
+import { expect } from "chai";
 
 describe('map()', function () {
-    it('todo')
 
-    // it('1 + 1 = 2', function () {
-    //     assert.equal(add(1, 1), 2);
-    // });
+    it("maps integer array to their squares", function () {
+        const arr = [4, 8];
+        function square(n) {
+          return n * n;
+        }
+        expect(map(arr, square)).to.deep.eql([16, 64]);
+    });
+    
+    // NEGATIVE CASES
+
+    it("throws an error if function cannot be applied to elements", function () {
+        const arr = [4, 8];
+        function length(n) {
+          return n.length;
+        }
+        expect(() => map(arr, length)).to.throw(Error);
+    });
 });
