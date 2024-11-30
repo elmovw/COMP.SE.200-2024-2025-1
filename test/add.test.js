a1aa5dff92dad61ca95aa36be3c52afd129caa6c
Elmo von Weissenberg <elmo.weissenberg@gmail.com>
2024-11-30 15:05:57 +0200

    add(): string cases

--- test/add.test.js
+++ test/add.test.js
@@ -3,15 +3,29 @@ import add from '../src/add.js'
 
 
 describe('add()', function () {
-  // a, b, result
-  const cases = [
-    [1, 1, 2],
-    [1, -1, 0],
-    [8900, 101, 9001]
-  ]
 
   it('basic numerical addition', function () {
-    for (const [a, b, result] of cases)
+    // a, b, result
+    const cases = [
+      [1, 1, 2],
+      [1, -1, 0],
+      [8900, 101, 9001]
+    ]
+
+    for (const [a, b, result] of cases) {
       assert.equal(add(a, b), result);
+    }
   });
+
+  it ('addition with strings', function () {
+    // a, b, result
+    const cases = [
+      [1, "1", "11"],
+      ["10", -1, "10-1"],
+      ["8900", "101", "8900101"]
+    ]
+    for (const [a, b, result] of cases) {
+      assert.equal(add(a, b), result);
+    }
+  })
 });
