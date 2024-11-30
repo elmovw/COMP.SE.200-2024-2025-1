f07d1a8d426b4102c4844829f49dd4a422dcf9d3
Elmo von Weissenberg <elmo.weissenberg@gmail.com>
2024-11-30 15:01:57 +0200

    add more cases to add() tests

--- test/add.test.js
+++ test/add.test.js
@@ -3,7 +3,15 @@ import add from '../src/add.js'
 
 
 describe('add()', function () {
-    it('1 + 1 = 2', function () {
-        assert.equal(add(1, 1), 2);
-    });
+  // a, b, result
+  const cases = [
+    [1, 1, 2],
+    [1, -1, 0],
+    [8900, 101, 9001]
+  ]
+
+  it('basic numerical addition', function () {
+    for (const [a, b, result] of cases)
+      assert.equal(add(a, b), result);
+  });
 });
